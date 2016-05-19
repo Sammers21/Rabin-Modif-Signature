5e2727073f97f42be44e3f3fa6e7983b2c40129b
Sammers21 <titantins@ya.ru>
2016-05-19 18:51:03 +0300

    init commit

---
+++ Rabin Window.BL/FileManager.cs
@@ -0,0 +1,79 @@
+﻿using System.IO;
+using System.Text;
+using RabinLib;
+using System.Numerics;
+using System.Linq;
+namespace Rabin_Window.BL
+{
+    public interface IFileManager
+    {
+        bool isExist(string filepath);
+        void SaveContent(string content, string filepath);
+        string GetContent(string path);
+        string GetContent(string path, BigInteger OpenKey, out bool[] Signs);
+        string GetContent(string path, Encoding encoding, BigInteger OpenKey, out bool[] Signs);
+        void SaveContent(string content, string filepath, BigInteger p, BigInteger q);
+        void SaveContent(string content, string filepath, Encoding encoding, BigInteger p, BigInteger q);
+        int GetSymbloCount(string content);
+    }
+
+    public class FileManager : IFileManager
+    {
+        private readonly Encoding _defaultEncoding = Encoding.GetEncoding(1251);
+
+        public bool isExist(string filepath)
+        {
+            return File.Exists(filepath);
+        }
+
+        public string GetContent(string path, BigInteger OpenKey, out bool[] Signs)
+        {
+            return GetContent(path, _defaultEncoding, OpenKey, out Signs);
+        }
+
+        public string GetContent(string path)
+        {
+            return File.ReadAllText(path, _defaultEncoding);
+        }
+
+        public string GetContent(string path, Encoding encoding, BigInteger OpenKey, out bool[] Signs)
+        {
+            Signature[] conten = File.ReadAllLines(path, encoding).Select(p =>
+            {
+                BigInteger[] te = p.Split(' ').Select(F => BigInteger.Parse(F)).ToArray();
+                return new Signature(te[0], te[1]);
+            }).ToArray();
+
+            string result = Rabin.DecryptionWithVertifBigText(conten, OpenKey, out Signs);
+
+            return result;
+        }
+
+        public void SaveContent(string content, string filepath, BigInteger p, BigInteger q)
+        {
+            SaveContent(content, filepath, _defaultEncoding, p, q);
+        }
+
+        public void SaveContent(string content, string filepath, Encoding encoding, BigInteger p, BigInteger q)
+        {
+            Signature[] EncrypArr = Rabin.RabinSignatureBigtext(content, p, q);
+
+
+            string[] res = EncrypArr.Select(P => P.S + " " + P.I).ToArray();
+
+            File.WriteAllLines(filepath, res, encoding);
+
+        }
+
+        public void SaveContent(string content, string filepath)
+        {
+            File.WriteAllText(filepath, content, _defaultEncoding);
+        }
+
+        public int GetSymbloCount(string content)
+        {
+            return content.Length;
+        }
+
+    }
+}
---
+++ Testsomelibs/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using RabinLib;
+using System.Numerics;
+
+namespace RabinTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            do
+            {
+
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Введиет тестиремый реджим \n" +
+                "1- обычная демонстрация\n" +
+                "2- подпись Рабина\n" +
+                "3- модифицированная подпись Рабина\n");
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                string Answer = Console.ReadLine();
+
+                switch (Answer)
+                {
+                    case "1":
+                        {
+
+                            Console.Clear();
+
+                            BigInteger p = 3004913, q = 20979403, n = p * q;
+
+
+                            Console.WriteLine("В данном случае используются p={0}\tq={1}", p, q);
+
+                            Console.WriteLine("Ведите текст для зашифровки\n");
+                            string text = Console.ReadLine();
+                            Console.WriteLine("\n");
+                            try
+                            {
+                                BigInteger[] arr = Rabin.EncryptionBigText(text, n);
+
+                                Console.WriteLine("\nBigInt Зашифрованное представление Массив :\n\n");
+                                foreach (BigInteger b in arr)
+                                {
+                                    Console.WriteLine("\t " + b);
+                                }
+                                Console.WriteLine("\n");
+                                string decrText = Rabin.DecryptionBigText(arr, p, q);
+
+
+                                Console.WriteLine("\n\n\nПри расшивровке было получено сообдещие:\n\n"
+                                     + decrText);
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                            }
+
+                        }
+                        break;
+                    case "2":
+                        {
+
+
+                            Console.Clear();
+
+                            Console.WriteLine("Ведите текст для зашифровки");
+
+                            string text = Console.ReadLine();
+
+
+                            BigInteger p = 3004913, q = 1109219;
+                            BigInteger n = p * q;
+
+                            Console.WriteLine("В данном случае используются p={0}\tq={1}", p, q);
+                            try
+                            {
+                                Signature[] S = Rabin.RabinSignatureBigtext(text, p, q);
+
+                                Console.WriteLine("\n");
+                                foreach (Signature numTe in S)
+                                    Console.WriteLine("Подпись S={0},I={1}", numTe.S, numTe.I);
+                                Console.WriteLine("\n");
+                                bool[] truestor;
+                                string dec = Rabin.DecryptionWithVertifBigText(S, n, out truestor);
+
+                                Console.WriteLine(dec);
+                                Console.WriteLine("\n");
+                                
+                                foreach(Boolean b in truestor)
+                                    Console.WriteLine(b);
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                            }
+
+                        }
+                        break;
+
+                    case "3":
+                        {
+
+                            Console.Clear();
+                            Console.WriteLine("Ведиет текст для зашифровки");
+                            string Text = Console.ReadLine();
+                            try
+                            {
+                                BigInteger p = 3795059, q = 3795007, n = q * p, SEcret = Rabin.CalcOfSecretKey(p, q);
+
+                                Console.WriteLine("Отрытый ключ n={0}  Secret key={1}", n, SEcret);
+
+                                BigInteger Sign = Rabin.ModifCalcSignatyre(Text, n, SEcret);
+                                Console.WriteLine("Подпись S= " + Sign);
+
+                                bool ans;
+                                string result = Rabin.DecryptSign(Sign, n, out ans);
+                                Console.WriteLine("Результат расшифровки " + result);
+                                Console.WriteLine("Подпись имеет значе " + ans);
+
+                                Console.ReadLine();
+                            }
+                            catch (Exception e)
+                            {
+
+                                Console.WriteLine(e.Message);
+                                Console.WriteLine("\n Не забыайте что ключи должны быть дотстаточно большими");
+                            }
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Не корректный ввод, попробуйте еще раз");
+                        break;
+                }
+                Console.WriteLine("\n\nдля выхода нажимте Esc");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+
+
+        }
+    }
+
+}
